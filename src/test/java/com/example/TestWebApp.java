dc7541abb8e6c48aab27fef3c2c4a860b1cbb214
bader boubaker <boubakerbader86@gmail.com>
2025-11-29 01:21:28 +0100

    #

---
+++ src/test/java/com/example/TestWebApp.java
@@ -0,0 +1,13 @@
+package com.example; // Adaptez au package de votre projet
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestWebApp {
+
+    @Test
+    public void testTrueCondition() {
+        // Assurez-vous que les tests sont bien dans le répertoire src/test/java/
+        assertTrue(true);
+    }
+}
